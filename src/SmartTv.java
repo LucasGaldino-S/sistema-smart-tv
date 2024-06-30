d9e0a51b918897524d103bd1e2325ac9b7592ca4
LucasGaldino-S <galdinosaraiva@gmail.com>
2024-06-29 21:17:40 -0300

    testando parametros

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -3,6 +3,28 @@ public class SmartTv {
   int canal = 1;
   int volume = 25;
 
+  public void escolherCanal(int novoCanal){
+    canal=novoCanal;
+  }
+
+  public void diminuirCanal(){
+    canal--;
+  }
+
+  public void aumentarCanal(){
+    canal++;
+  }
+
+  public void aumentarVolume(){
+  volume++;
+  System.out.println("Volume :" + volume);
+}
+
+  public void diminuirVolume(){
+    volume--;
+    System.out.println("Volume :" + volume);
+  }
+
   public void ligar(){
     ligada=true;
 
